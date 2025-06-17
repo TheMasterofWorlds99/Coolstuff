d009c1de361c17a7de5764a4b6de07b00266439d
TheMasterofWorlds99 <162048515+TheMasterofWorlds99@users.noreply.github.com>
2025-06-17 17:39:02 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,25 +6,25 @@
 
 template<typename T>
 struct Test {
-	T value; //NOTE TO SELF: auto doesn't work, because it has to be a type, not a value. auto would work if we defined it in the struct like: auto value = 23;
+  T value; //NOTE TO SELF: auto doesn't work, because it has to be a type, not a value. auto would work if we defined it in the struct like: auto value = 23;
 
-	Test(T val) : value(val) {} // Constructor to initialize value
+  Test(T val) : value(val) {} // Constructor to initialize value
 
-	void display() const {
-		std::cout << "Value: " << value << std::endl;
-	}
+  void display() const {
+	  std::cout << "Value: " << value << std::endl;
+  }
 };
 
 int main() {
-	//testing the -> syntax in c++
-	std::unique_ptr<Test<int>> testPtr = std::make_unique<Test<int>>(42);  // Dynamically allocate a Test<int> object (using int)
-	std::unique_ptr<Test<float>> testPtrfloat = std::make_unique<Test<float>>(42.12f); // Dynamically allocate a Test<float> object (using float)
-	std::unique_ptr<Test<char>> testPtrchar = std::make_unique<Test<char>>('A'); // Dynamically allocate a Test<char> object (using char)
+  //testing the -> syntax in c++
+  std::unique_ptr<Test<int>> testPtr = std::make_unique<Test<int>>(42);  // Dynamically allocate a Test<int> object (using int)
+  std::unique_ptr<Test<float>> testPtrfloat = std::make_unique<Test<float>>(42.12f); // Dynamically allocate a Test<float> object (using float)
+  std::unique_ptr<Test<char>> testPtrchar = std::make_unique<Test<char>>('A'); // Dynamically allocate a Test<char> object (using char)
 
-	testPtr->display();  // Using -> to access member function
-	testPtrfloat->display();  // Using -> to access member function
-	testPtrchar->display();  // Using -> to access member function
+  testPtr->display();  // Using -> to access member function
+  testPtrfloat->display();  // Using -> to access member function
+  testPtrchar->display();  // Using -> to access member function
 
-	// No need for delete for unique_ptr as it automatically manages memory
-	return 0;
+  // No need for delete for unique_ptr as it automatically manages memory
+  return 0;
 }
